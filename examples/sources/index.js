b932ec28fca332e362bb751a96cf2dd886f4fda6
Eponesh <s.eponeshnikov@gmail.com>
2019-01-22 22:13:48 +0700

    Add example

---
+++ examples/sources/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import IconFirst from './icons/icon-1.svg';
+import IconSecond from './icons/icon-2.svg';
+import IconThird from './icons/icon-3.svg';
+
+const components = {
+    IconFirst,
+    IconSecond,
+    IconThird
+};
+
+new Vue({
+    el: '#app',
+    data: {},
+    components
+});
