e2cb7d5f6f80cccfeecd12596248d51a58b9c788
Eponesh <s.eponeshnikov@gmail.com>
2019-01-22 21:47:58 +0700

    Changes template component generation to functional

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const loaderUtils = require('loader-utils');
-const getComponent = require('./lib/getComponent');
+
+/**
+ * Helpful styles to provide coloring and fit svg
+ */
+const SVG_STYLE = {
+    width: '100%',
+    height: '100%',
+    fill: 'currentColor'
+};
 
 /**
  * Takes svg converted source from "svg-sprite-loader"
@@ -18,8 +26,16 @@ module.exports = function (source) {
         customClass = ''
     } = loaderUtils.getOptions(this) || {};
 
-    return `${source.replace(/export.*/, '')}
-        var component = ${getComponent(id, withStyle, customClass)};
+    const style = JSON.stringify(withStyle ? SVG_STYLE : {});
+
+    const resolvedPath = require.resolve("./lib/createSvgComponent");
+    const importPath = loaderUtils.stringifyRequest(this, resolvedPath);
+
+    const modifiedSource = `${source.replace(/export.*/, '')}
+        import createSvgComponent from ${importPath};
+        var component = createSvgComponent("#${id}", ${style}, "${customClass}");
         export default component;
     `;
+
+    return modifiedSource;
 }
